ca97810c698ec160d98f78b1630f8a0eb657091b
Sören Klein <soerenklein98@protonmail.com>
2022-09-03 17:33:24 +0200

    finish code coverage

--- src/Type/Node.php
+++ src/Type/Node.php
@@ -8,6 +8,7 @@ use Syndesi\CypherDataStructures\Contract\NodeInterface;
 use Syndesi\CypherDataStructures\Contract\NodeLabelInterface;
 use Syndesi\CypherDataStructures\Contract\NodeLabelStorageInterface;
 use Syndesi\CypherDataStructures\Contract\RelationInterface;
+use Syndesi\CypherDataStructures\Contract\WeakRelationInterface;
 use Syndesi\CypherDataStructures\Contract\WeakRelationStorageInterface;
 use Syndesi\CypherDataStructures\Exception\InvalidArgumentException;
 use Syndesi\CypherDataStructures\Helper\ToCypherHelper;
@@ -79,31 +80,60 @@ class Node implements NodeInterface
     /**
      * @throws InvalidArgumentException
      */
-    public function addRelation(RelationInterface $relation): self
+    public function addRelation(WeakRelationInterface|RelationInterface $relation): self
     {
+        $weakRelation = $relation;
+        if ($weakRelation instanceof RelationInterface) {
+            $weakRelation = WeakRelation::create($weakRelation);
+        }
+        if (null === $weakRelation->get()) {
+            throw InvalidArgumentException::createForAlreadyNullReference(WeakRelationInterface::class);
+        }
+        /** @psalm-suppress PossiblyNullReference */
+        if (null === $weakRelation->get()->getStartNode()) {
+            throw InvalidArgumentException::createForTypeMismatch(NodeInterface::class, 'null');
+        }
+        /** @psalm-suppress PossiblyNullReference */
+        if (null === $weakRelation->get()->getEndNode()) {
+            throw InvalidArgumentException::createForTypeMismatch(NodeInterface::class, 'null');
+        }
         $ownIdentifyingString = ToCypherHelper::nodeToIdentifyingCypherString($this);
-        if (ToCypherHelper::nodeToIdentifyingCypherString($relation->getStartNode()) !== $ownIdentifyingString &&
-            ToCypherHelper::nodeToIdentifyingCypherString($relation->getEndNode()) !== $ownIdentifyingString
+        /** @psalm-suppress PossiblyNullReference */
+        if (ToCypherHelper::nodeToIdentifyingCypherString($weakRelation->get()->getStartNode()) !== $ownIdentifyingString &&
+            ToCypherHelper::nodeToIdentifyingCypherString($weakRelation->get()->getEndNode()) !== $ownIdentifyingString
         ) {
             throw new InvalidArgumentException("Adding a relation to a node requires that either the start node or the end node must be the same as the node itself.");
         }
 
-        $this->weakRelationStorage->attach($relation);
+        $this->weakRelationStorage->attach($weakRelation);
 
         return $this;
     }
 
+    /**
+     * @throws InvalidArgumentException
+     */
     public function addRelations(WeakRelationStorageInterface $weakRelationStorage): self
     {
         foreach ($weakRelationStorage as $key) {
-            $this->weakRelationStorage->attach($key);
+            $this->addRelation($key);
         }
 
         return $this;
     }
 
-    public function hasRelation(RelationInterface $relation): bool
+    /**
+     * @throws InvalidArgumentException
+     */
+    public function hasRelation(WeakRelationInterface|RelationInterface $relation): bool
     {
+        if ($relation instanceof RelationInterface) {
+            $relation = WeakRelation::create($relation);
+        }
+        if (null === $relation->get()) {
+            throw InvalidArgumentException::createForAlreadyNullReference(WeakRelationInterface::class);
+        }
+
         return $this->weakRelationStorage->contains($relation);
     }
 
@@ -112,8 +142,17 @@ class Node implements NodeInterface
         return $this->weakRelationStorage;
     }
 
-    public function removeRelation(RelationInterface $relation): self
+    /**
+     * @throws InvalidArgumentException
+     */
+    public function removeRelation(WeakRelationInterface|RelationInterface $relation): self
     {
+        if ($relation instanceof RelationInterface) {
+            $relation = WeakRelation::create($relation);
+        }
+        if (null === $relation->get()) {
+            throw InvalidArgumentException::createForAlreadyNullReference(WeakRelationInterface::class);
+        }
         $this->weakRelationStorage->detach($relation);
 
         return $this;
--- tests/Type/NodeTest.php
+++ tests/Type/NodeTest.php
@@ -6,10 +6,15 @@ namespace Syndesi\CypherDataStructures\Tests\Type;
 
 use PHPUnit\Framework\TestCase;
 use stdClass;
+use Syndesi\CypherDataStructures\Exception\InvalidArgumentException;
 use Syndesi\CypherDataStructures\Type\Node;
 use Syndesi\CypherDataStructures\Type\NodeLabel;
 use Syndesi\CypherDataStructures\Type\NodeLabelStorage;
 use Syndesi\CypherDataStructures\Type\PropertyName;
+use Syndesi\CypherDataStructures\Type\Relation;
+use Syndesi\CypherDataStructures\Type\RelationType;
+use Syndesi\CypherDataStructures\Type\WeakRelation;
+use Syndesi\CypherDataStructures\Type\WeakRelationStorage;
 
 class NodeTest extends TestCase
 {
@@ -71,5 +76,195 @@ class NodeTest extends TestCase
         $this->assertFalse($nodeC->isEqualTo($nodeA));
         $this->assertFalse($nodeA->isEqualTo(new stdClass()));
         $this->assertFalse($nodeA->isEqualTo('some string'));
+    }
+
+    public function testRelations(): void
+    {
+        $node = new Node();
+        $node->addNodeLabel(new NodeLabel('Node'));
+        $node->addProperty(new PropertyName('id'), 1234);
+        $node->addIdentifier(new PropertyName('id'));
+
+        $otherNode = new Node();
+        $otherNode->addNodeLabel(new NodeLabel('OtherNode'));
+        $otherNode->addProperty(new PropertyName('id'), 4321);
+        $otherNode->addIdentifier(new PropertyName('id'));
+
+        $relationA = new Relation();
+        $relationA->setStartNode($node);
+        $relationA->setEndNode($otherNode);
+        $relationA->setRelationType(new RelationType('TYPE'));
+
+        $relationB = new Relation();
+        $relationB->setStartNode($otherNode);
+        $relationB->setEndNode($node);
+        $relationB->setRelationType(new RelationType('TYPE'));
+
+        $relationC = new Relation();
+        $relationC->setStartNode($node);
+        $relationC->setEndNode($node);
+        $relationC->setRelationType(new RelationType('TYPE'));
+
+        $node->addRelation($relationA);
+        $this->assertSame(1, $node->getRelations()->count());
+        $this->assertTrue($node->hasRelation($relationA));
+
+        $node->addRelation($relationB);
+        $this->assertSame(2, $node->getRelations()->count());
+        $this->assertTrue($node->hasRelation($relationB));
+
+        $node->addRelation($relationC);
+        $this->assertSame(3, $node->getRelations()->count());
+        $this->assertTrue($node->hasRelation($relationC));
+
+        $node->removeRelation($relationA);
+        $this->assertFalse($node->hasRelation($relationA));
+        $this->assertSame(2, $node->getRelations()->count());
+
+        $node->clearRelations();
+        $this->assertFalse($node->hasRelation($relationB));
+        $this->assertSame(0, $node->getRelations()->count());
+
+        $weakRelationStorage = new WeakRelationStorage();
+        $weakRelationStorage->attach(WeakRelation::create($relationA));
+        $weakRelationStorage->attach(WeakRelation::create($relationB));
+        $weakRelationStorage->attach(WeakRelation::create($relationC));
+        $node->addRelations($weakRelationStorage);
+        $this->assertSame(3, $node->getRelations()->count());
+    }
+
+    public function testAddInvalidRelation(): void
+    {
+        if (false !== getenv("LEAK")) {
+            $this->markTestSkipped();
+        }
+
+        $node = new Node();
+        $node->addNodeLabel(new NodeLabel('Node'));
+        $node->addProperty(new PropertyName('id'), 1234);
+        $node->addIdentifier(new PropertyName('id'));
+
+        $otherNode = new Node();
+        $otherNode->addNodeLabel(new NodeLabel('OtherNode'));
+        $otherNode->addProperty(new PropertyName('id'), 4321);
+        $otherNode->addIdentifier(new PropertyName('id'));
+
+        $relation = new Relation();
+        $relation->setStartNode($node);
+        $relation->setEndNode($otherNode);
+        $relation->setRelationType(new RelationType('TYPE'));
+
+        $weakRelation = WeakRelation::create($relation);
+        unset($relation);
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage("Reference of type 'Syndesi\CypherDataStructures\Contract\WeakRelationInterface' is already null.");
+
+        $node->addRelation($weakRelation);
+    }
+
+    public function testAddRelationWithoutBeingStartOrEnd(): void
+    {
+        if (false !== getenv("LEAK")) {
+            $this->markTestSkipped();
+        }
+
+        $node = new Node();
+        $node->addNodeLabel(new NodeLabel('Node'));
+        $node->addProperty(new PropertyName('id'), 1234);
+        $node->addIdentifier(new PropertyName('id'));
+
+        $otherNode = new Node();
+        $otherNode->addNodeLabel(new NodeLabel('OtherNode'));
+        $otherNode->addProperty(new PropertyName('id'), 4321);
+        $otherNode->addIdentifier(new PropertyName('id'));
+
+        $relation = new Relation();
+        $relation->setStartNode($otherNode);
+        $relation->setEndNode($otherNode);
+        $relation->setRelationType(new RelationType('TYPE'));
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage("Adding a relation to a node requires that either the start node or the end node must be the same as the node itself.");
+
+        $node->addRelation($relation);
+    }
+
+    public function testAddRelationWithoutStartNode(): void
+    {
+        if (false !== getenv("LEAK")) {
+            $this->markTestSkipped();
+        }
+
+        $node = new Node();
+        $node->addNodeLabel(new NodeLabel('Node'));
+        $node->addProperty(new PropertyName('id'), 1234);
+        $node->addIdentifier(new PropertyName('id'));
+
+        $relation = new Relation();
+        $relation->setEndNode($node);
+        $relation->setRelationType(new RelationType('TYPE'));
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage("Expected type 'Syndesi\CypherDataStructures\Contract\NodeInterface', got type 'null'");
+
+        $node->addRelation($relation);
+    }
+
+    public function testAddRelationWithoutEndNode(): void
+    {
+        if (false !== getenv("LEAK")) {
+            $this->markTestSkipped();
+        }
+
+        $node = new Node();
+        $node->addNodeLabel(new NodeLabel('Node'));
+        $node->addProperty(new PropertyName('id'), 1234);
+        $node->addIdentifier(new PropertyName('id'));
+
+        $relation = new Relation();
+        $relation->setStartNode($node);
+        $relation->setRelationType(new RelationType('TYPE'));
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage("Expected type 'Syndesi\CypherDataStructures\Contract\NodeInterface', got type 'null'");
+
+        $node->addRelation($relation);
+    }
+
+    public function testHasInvalidRelation(): void
+    {
+        if (false !== getenv("LEAK")) {
+            $this->markTestSkipped();
+        }
+
+        $node = new Node();
+
+        $relation = new Relation();
+        $weakRelation = WeakRelation::create($relation);
+        unset($relation);
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage("Reference of type 'Syndesi\CypherDataStructures\Contract\WeakRelationInterface' is already null.");
+
+        $node->hasRelation($weakRelation);
+    }
+
+    public function testRemoveInvalidRelation(): void
+    {
+        if (false !== getenv("LEAK")) {
+            $this->markTestSkipped();
+        }
+
+        $node = new Node();
+
+        $relation = new Relation();
+        $weakRelation = WeakRelation::create($relation);
+        unset($relation);
+
+        $this->expectException(InvalidArgumentException::class);
+        $this->expectExceptionMessage("Reference of type 'Syndesi\CypherDataStructures\Contract\WeakRelationInterface' is already null.");
+
+        $node->removeRelation($weakRelation);
     }
 }
