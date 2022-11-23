1fb3e47daa262721a846a92cfe0f3c36a5185a82
Syndesi <soerenklein98@gmail.com>
2022-11-23 21:30:50 +0100

    wip

--- tests/Type/NodeTest.php
+++ tests/Type/NodeTest.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Syndesi\CypherDataStructures\Tests\Type;
 
 use PHPUnit\Framework\TestCase;
+use Syndesi\CypherDataStructures\Exception\InvalidArgumentException;
 use Syndesi\CypherDataStructures\Type\Node;
 use Syndesi\CypherDataStructures\Type\Relation;
 
@@ -90,5 +91,60 @@ class NodeTest extends TestCase
 
         $node->removeRelations();
         $this->assertEmpty($node->getRelations());
+    }
+
+    public function testExceptionOnMissingStartNode(): void
+    {
+        $node = new Node();
+        $relation = (new Relation())
+            ->setEndNode(new Node());
+        $this->expectExceptionMessage('Start node must be set');
+        $this->expectException(InvalidArgumentException::class);
+        $node->addRelation($relation);
+    }
+
+    public function testExceptionOnMissingEndNode(): void
+    {
+        $node = new Node();
+        $relation = (new Relation())
+            ->setStartNode(new Node());
+        $this->expectExceptionMessage('End node must be set');
+        $this->expectException(InvalidArgumentException::class);
+        $node->addRelation($relation);
+    }
+
+    public function testExceptionOnRelationIsNotConnectedToNode(): void
+    {
+        $node = (new Node())
+            ->addProperty('id', 1234)
+            ->addIdentifier('id');
+        $relation = (new Relation())
+            ->setStartNode(new Node())
+            ->setEndNode(new Node());
+        $this->expectExceptionMessage('Adding a relation to a node requires that either the start node or the end node must be the same as the node itself.');
+        $this->expectException(InvalidArgumentException::class);
+        $node->addRelation($relation);
+    }
+
+    public function testToString(): void
+    {
+        $node = (new Node())
+            ->addLabels(['LabelA', 'LabelZ', 'LabelC', 'discouraged Style'])
+            ->addProperty('id', 123)
+            ->addIdentifier('id');
+        // todo fix backticks in ToCypherHelper class
+        $this->assertSame("(:LabelA:LabelC:LabelZ:discouraged Style {id: '123'})", (string) $node);
+    }
+
+    public function testIsEqualTo(): void
+    {
+        $node = (new Node())
+            ->addLabel('Node')
+            ->addProperty('id', 123)
+            ->addIdentifier('id');
+        $this->assertFalse($node->isEqualTo(123));
+        $this->assertFalse($node->isEqualTo(new Node()));
+        $this->assertTrue($node->isEqualTo($node));
+        $this->assertTrue($node->isEqualTo(clone $node));
     }
 }
